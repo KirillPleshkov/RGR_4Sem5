41777105929cf8fac67bb540ac81c6ef21d21616
kirill <kirypleshkov@mail.ru>
2023-05-12 13:50:42 +0700

    3

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {Navigate, Route, Routes} from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import Configurator from "./pages/Configurator";
+
+function App() {
+  return (
+        <Routes>
+
+            <Route path="/" element={<HomePage/>}/>
+
+            <Route path="/configurator" element={<Configurator/>}/>
+
+            <Route path="/*" element={<Navigate to={"/"}/>} />
+
+        </Routes>
+  );
+}
+
+export default App;
